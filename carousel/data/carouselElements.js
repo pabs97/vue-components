f48d52b8fa37a7eb1678e10afa479da903470f6f
Pablo Suarez <pablo.suarez@macys.com>
2020-03-15 20:06:28 -0700

    adding components

---
+++ carousel/data/carouselElements.js
@@ -0,0 +1,95 @@
+const carouselElementsDataFile = [
+  {
+    imageUrl: "https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126-1024x683.jpg",
+    title: "kitten 0",
+    description: "this is Albert"
+  },
+  {
+    imageUrl: "https://www.aspca.org/sites/default/files/blog_foster-myth_062718_main.jpg",
+    title: "kitten 1",
+    description: "this is Bubbles"
+  },
+  {
+    imageUrl: "https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126-1024x683.jpg",
+    title: "kitten 2",
+    description: "this is Caramel"
+  },
+  {
+    imageUrl: "https://www.aspca.org/sites/default/files/blog_foster-myth_062718_main.jpg",
+    title: "kitten 3",
+    description: "this is Doodles"
+  },
+  {
+    imageUrl: "https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126-1024x683.jpg",
+    title: "kitten 4",
+    description: "this is Ellie"
+  },
+  {
+    imageUrl: "https://www.aspca.org/sites/default/files/blog_foster-myth_062718_main.jpg",
+    title: "kitten 5",
+    description: "this is Fluffy"
+  },
+  {
+    imageUrl: "https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126-1024x683.jpg",
+    title: "kitten 6",
+    description: "this is Gatita"
+  },
+  {
+    imageUrl: "https://www.aspca.org/sites/default/files/blog_foster-myth_062718_main.jpg",
+    title: "kitten 7",
+    description: "this is Hercules"
+  },
+];
+
+const carouselElementsDataFile2 = [
+  {
+    title: 'Besties',
+    description: 'Blue, Navi & Lena',
+    imageUrl: './assets/DSC01373.jpg'
+  },
+  {
+    title: 'Pumpkin Patch',
+    description: 'First time going to a pumpkin patch.',
+    imageUrl: './assets/DSC01805.JPG'
+  },
+  {
+    title: 'Pumpkin Patch',
+    description: 'Looking for the perfect pumpkin.',
+    imageUrl: './assets/DSC01865.JPG'
+  },
+  {
+    title: 'First Road Trip',
+    description: 'Rest area near Mt Shasta',
+    imageUrl: './assets/LRG_DSC00137.JPG'
+  },
+  {
+    title: 'Backyard',
+    description: 'Enjoying the Washington weather.',
+    imageUrl: './assets/LRG_DSC00206.JPG'
+  },
+  {
+    title: 'Lake',
+    description: 'Enjoying the view of a lake in Washington.',
+    imageUrl: './assets/LRG_DSC00297.JPG'
+  },
+  {
+    title: 'Rattlesnake Lake',
+    description: 'Enjoying Washington',
+    imageUrl: './assets/LRG_DSC00631.jpg'
+  },
+  {
+    title: 'Lands End',
+    description: 'Lands End Trail in SF.',
+    imageUrl: './assets/LRG_DSC01078.jpg'
+  },
+  {
+    title: 'Dog Park',
+    description: 'Enjoying the dog waterfall play area.',
+    imageUrl: './assets/IMG_6553.JPG'
+  },
+  {
+    title: "Santa's Little Helper",
+    description: 'With her elf toy. (Not a fan of headbands)',
+    imageUrl: './assets/DSC03309.JPG'
+  },
+];
